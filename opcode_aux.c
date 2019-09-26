90b0827b2e3c94955ff5c4a71e66335715ba33c1
Caroll1889 <diahancaroll@hotmail.com>
2019-09-26 21:04:36 +0000

    Feature: auxiliar functions

--- opcode_aux.c
+++ opcode_aux.c
@@ -17,3 +17,91 @@ int _isdigit(int c)
 		return (0);
 	}
 }
+
+/**
+ * sub - substracts the top element of the stack from the second top element.
+ * @stack: Double linked list.
+ * @line_number: .
+ * Return: Nothing
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int new_val;
+
+	if (temp == NULL || *temp->next == NULL)
+	{
+		fprintf(stderr, "L%u: can’t sub, stack too short\n", line_number)
+		exit(EXIT_FAILURE)
+	}
+	new_val = temp->next->n - temp->n;
+	temp->next->n = new_val;
+
+	pop(stack, line_number);
+}
+
+
+/**
+ * mul - multiply the second top element of the stack with the top element.
+ * @stack: Double linked list.
+ * @line_number: line_number.
+ * Return: Nothing
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp
+	int new_val
+
+	temp = *stack
+
+	if (temp == NULL || *temp->next == NULL)
+	{
+		fprintf(stderr, "L%u: can’t mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE)
+	}
+	new_val = temp->next->n * temp->n;
+	temp->next->n = new_val;
+
+	pop(stack, line_number)
+}
+
+/**
+ * nop - doesn't do anything
+ * @stack: Double linked list.
+ * @line_number: line_number.
+ * Return: Nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	void(line_number)
+	void(stack)
+}
+
+/**
+ * add - adds the top two elements of the stack.
+ * @stack: Double linked list.
+ * @line_number: line_number.
+ * Return: Nothing
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+	stack_t result;
+
+	if (*stack < 2)
+	{
+		fprinft(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		sum = result->n;
+		result = sum->(*stack) + next->n;
+		result = sum;
+		pop(stack, line_number);
+	}
+}

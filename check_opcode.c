2d60d069d2a3c80b0dab446b733f17a4f86ce3ac
Caroll1889 <diahancaroll@hotmail.com>
2019-09-26 20:58:20 +0000

    Feature: struct

--- check_opcode.c
+++ check_opcode.c
@@ -11,14 +11,27 @@
 void check_opcode(stack_t **stack, char *token, unsigned int line_number)
 {
 	instruction_t opcode_op[] = {
-		{“push”, push},
-		{“pall”, pul},
-		{“print”, pint},
-		{“pop”, pop},
-		{“swap”, swap},
-		{“add”, add},
-		{“nop”, nop},
+		{"push", push},
+		{"pall", pul},
+		{"print", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
 		{"sub", sub},
 		{"mul", mul}
 		{NULL, NULL};
+	};
+	i = 0;
+	while (opcode_op[i].opcode_op != NULL)
+	{
+		if (strcmp(opcode_op[i].opcode_op, token) == 0)
+		{
+			opcode_op[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, token);
+	exit(EXIT_FAILURE);
 }

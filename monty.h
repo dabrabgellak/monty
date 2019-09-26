f5669ed3d8e84abbb88527d88b22020b703eb6ec
dabrabgellak <barbaracalle10i3@gmail.com>
2019-09-26 21:21:08 +0000

    feature: included more libraries

--- monty.h
+++ monty.h
@@ -2,6 +2,8 @@
 #define _MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -37,8 +39,8 @@ typedef struct instruction_s
 
 void check_opcode(stack_t **stack, char *token, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
-void pall(stck_t **stack, unsigned int line_number);
-void pint(stck_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
